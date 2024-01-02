6fccedc97424e0a09526946cab01ab4963d19594
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-01-02 09:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 app.set('view engine', 'ejs');
 
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({
         status: "SUCESS",
         message: "All Good"
