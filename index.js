934b513ebbf1616d70eeafcc8a4f9c1621cf0c26
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-01-02 14:08:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-//     {
-//         origin:"https://deft-cassata-c4d67b.netlify.app",
-//         methods:"POST","GET","PUT",
-//         credentials:true
-//        "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-// ));
+app.use(cors(
+    {
+        // origin:"https://deft-cassata-c4d67b.netlify.app",
+        // methods:"POST","GET","PUT",
+        // credentials:true
+       "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+));
 
 app.use(bodyParser.json())
 app.use(express.json())
